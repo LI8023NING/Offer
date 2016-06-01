c74b460468e955a2600b7b44da6fbf2e24f93e87
SeeYouTomorrow <li_ning_@outlook.com>
2016-06-02 00:46:35 +0800

    出栈的合法性判断，给两个序列，判断第二个序列是不是第一个序列的入栈又出栈的序列，给出bool返回值

---
+++ 出栈序列合法性/CheakStackTrue.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include<iostream>
+#include<iterator>
+using namespace std;
+#include<stack>
+
+//判断出栈序列的合法性 push入栈序列 pop待判断序列
+bool IsPopOrder(const int* push, const int* pop, int length)
+{
+	if (push == nullptr || pop == nullptr || length <= 0)
+		return false;
+
+	const int* ppush = push;
+	const int* ppop = pop;
+
+	std::stack<int> st;      //调用临时辅助栈
+	while (ppop - pop < length)  //大前提，pop序列没有到达最后，判断完成，以此为准
+	{
+		while (st.empty() || st.top() != *ppop)
+		{
+			if (ppush - push == length) //如果pop中的数 在push中根本没有呢
+				break;
+			st.push(*ppush);
+			ppush++;
+		}
+
+		if (st.top() == *ppop)
+		{
+			st.pop();
+			ppop++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if (st.empty() && ppop - pop == length)
+		return true;
+	else
+		return false;
+}
---
+++ 出栈序列合法性/MainTest.cpp
@@ -0,0 +1,24 @@
+#include"CheakStackTrue.h"
+
+void Test()
+{
+	int a[5] = {1,2,3,4,5};
+	int b[5] = {5,4,3,1,2};
+	
+	bool num = 0;
+	num = IsPopOrder(a,b,5);
+	if (num == true)
+	{
+		cout << "合法" << endl;
+	}
+	else
+	{
+		cout << "非法" << endl;
+	}
+}
+
+int main()
+{
+	Test();
+	return 0;
+}
