8c2ae093422ddd0a0eea8526ae8aeaf3ac610c38
SeeYouTomorrow <li_ning_@outlook.com>
2016-06-02 01:04:04 +0800

    改变if判断的结构 用一个bPossbile变量返回结果

--- 出栈序列合法性/CheakStackTrue.h
+++ 出栈序列合法性/CheakStackTrue.h
@@ -8,36 +8,37 @@ using namespace std;
 //判断出栈序列的合法性 push入栈序列 pop待判断序列
 bool IsPopOrder(const int* push, const int* pop, int length)
 {
-	if (push == nullptr || pop == nullptr || length <= 0)
-		return false;
+	bool bPossible = false;
 
-	const int* ppush = push;
-	const int* ppop = pop;
-
-	std::stack<int> st;      //调用临时辅助栈
-	while (ppop - pop < length)  //大前提，pop序列没有到达最后，判断完成，以此为准
+	if (push != nullptr && pop != nullptr && length > 0)
 	{
-		while (st.empty() || st.top() != *ppop)
+		const int* ppush = push;
+		const int* ppop = pop;
+
+		std::stack<int> st;      //调用临时辅助栈
+		while (ppop - pop < length)  //大前提，pop序列没有到达最后，判断完成，以此为准
 		{
-			if (ppush - push == length) //如果pop中的数 在push中根本没有呢
+			while (st.empty() || st.top() != *ppop)
+			{
+				if (ppush - push == length) //如果pop中的数 在push中根本没有呢
+					break;
+				st.push(*ppush);
+				ppush++;
+			}
+
+			if (st.top() == *ppop)
+			{
+				st.pop();
+				ppop++;
+			}
+			else
+			{
 				break;
-			st.push(*ppush);
-			ppush++;
+			}
 		}
 
-		if (st.top() == *ppop)
-		{
-			st.pop();
-			ppop++;
-		}
-		else
-		{
-			break;
-		}
+		if (st.empty() && ppop - pop == length)
+			bPossible = true;
 	}
-
-	if (st.empty() && ppop - pop == length)
-		return true;
-	else
-		return false;
+	return bPossible;
 }
--- 出栈序列合法性/MainTest.cpp
+++ 出栈序列合法性/MainTest.cpp
@@ -2,8 +2,8 @@
 
 void Test()
 {
-	int a[5] = {1,2,3,4,5};
-	int b[5] = {5,4,3,1,2};
+	int a[5] = { 1, 2, 3, 4, 5 };
+	int b[5] = { 5, 4, 3, 1, 2 };
 	
 	bool num = 0;
 	num = IsPopOrder(a,b,5);
